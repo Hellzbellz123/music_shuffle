b842cef4d739637676874dcd9034d026c699cea9
hellzbellz123 <smith.j.taylor2001@gmail.com>
2022-01-25 13:32:25 -0800

    suggested change for simplification, thanks plecra

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use std::{env, fs};
-use std::fs::{File};
-use std::io::{BufRead, BufReader, self, Write};
 use rand::seq::SliceRandom;
 use rand::thread_rng;
-
-
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
+use std::{env, fs};
 
 fn main() {
     let input = env::args().nth(1);
-    let reader: Box<dyn BufRead> = match input {
-        None => Box::new(BufReader::new(io::stdin())),
-        Some(filename) => Box::new(BufReader::new(fs::File::open(filename).unwrap()))
-
+    let stdin;
+    let reader: Box<dyn BufRead + '_> = match input {
+        None => {
+            stdin = io::stdin();
+            Box::new(stdin.lock())
+        }
+        Some(filename) => Box::new(BufReader::new(fs::File::open(filename).unwrap())),
     };
+
     let mut lines: Vec<String> = Vec::new();
     for line in reader.lines() {
         lines.push(line.unwrap());
@@ -21,7 +23,7 @@ fn main() {
 
     let mut outputfile = File::create("output.txt").expect("Unable to write file!");
 
-    for line in lines{
+    for line in lines {
         writeln!(outputfile, "{}", line).expect("There was an Error");
     }
 }
