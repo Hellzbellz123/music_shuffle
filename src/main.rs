e55c6db3ab1def1b711a579185ba05fc7f7b2541
hellzbellz123 <smith.j.taylor2001@gmail.com>
2022-02-05 12:09:15 -0800

    trying to implement pulling of playlist from spotify

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,61 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use serde::Deserialize;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write};
 use std::{env, fs};
+use rspotify::client::Spotify;
+use rspotify::oauth2::{SpotifyClientCredentials, SpotifyOAuth};
+use rspotify::util::get_token;
+use serde_derive::{Deserialize, Serialize};
 
-fn main() {
+extern crate rspotify;
+
+
+#[derive(Serialize, Deserialize)]
+struct PersistantData {
+    client_id: String,
+    client_secret: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let oauth = SpotifyOAuth::default()
+        .client_id("this-is-my-client-id")
+        .client_secret("this-is-my-client-secret")
+        .redirect_uri("http://localhost:8888/callback")
+        .build();
+
+    let client_credential = SpotifyClientCredentials::default()
+        .client_id("this-is-my-client-id")
+        .client_secret("this-is-my-client-secret")
+        .build();
+
+    let mut spotify_oauth = SpotifyOAuth::default().build();
+    match get_token(&mut spotify_oauth).await {
+        Some(token_info) => {
+            let client_credential = SpotifyClientCredentials::default()
+                .token_info(token_info)
+                .build();
+
+
+            let spotify = Spotify::default()
+                .client_credentials_manager(client_credential)
+                .build();
+            let user_id = "spotify";
+            let mut playlist_id = String::from("59ZbFPES4DQwEjBpWHzrtC");
+            let playlists = spotify
+                .user_playlist(user_id, Some(&mut playlist_id), None, None)
+                .await;
+            println!("{:?}", playlists);
+        }
+        None => println!("auth failed"),
+    };
+}
+
+
+fn readfile() {
+
     let input = env::args().nth(1);
     let stdin;
     let reader: Box<dyn BufRead + '_> = match input {
